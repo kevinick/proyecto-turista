ef68a18842175c3e52a033e8d23bb3ec365f9287
Diego Garcia <qtimpot@gmail.com>
2016-12-15 20:01:00 -0400

    cambio de html a jade

---
+++ app.js
@@ -0,0 +1,33 @@
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+app.set("view engine", "jade");
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function (req, res) {
+
+    res.render("index");
+});
+
+app.get("/login", function(req, res) {
+
+    res.render("login");
+});
+
+app.post("/login/valid", function(req, res) {
+
+    console.log(req.body.username);
+    console.log(req.body.password);
+    res.redirect("/");
+});
+
+app.get("/place/*", function(req, res) {
+
+    res.render("place");
+});
+
+app.listen(8080);
