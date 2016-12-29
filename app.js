2151a3f22a5cc324609106912e044c18114043aa
Diego Garcia <qtimpot@gmail.com>
2016-12-29 18:13:49 -0400

    backbone of place/:id done

--- app.js
+++ app.js
@@ -23,7 +23,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res) {
 
-    res.render("index", {places: []});
+    Place.find({}).limit(8).exec(function(err, data) {
+        iterateAndShowPlaces(0, data, function() {
+            res.render("index", {places: data});
+        });
+    });
 });
 
 app.get("/login", function(req, res) {
@@ -46,35 +50,10 @@ app.get("/new-place", function(req, res) {
 app.post("/new-place/save", multipartMiddleware, function(req, res) {
 
     User.findOne({name: "admin"}, function(err, user) {
+
         if (!user) {console.log("No existe usuario admin"); return;}
         if (err) {console.log(err); return;}
 
-        function newPlace(img) {
-            Place.create({
-                name: req.body.name,
-                description: req.body.description,
-                location: {
-                    city: req.body.city,
-                    zone: req.body.zone,
-                    street: req.body.street,
-                },
-                latlng: {
-                    latitude: req.body.latitude,
-                    longitude: req.body.longitude
-                },
-                images: img ? [img._id]:[],
-                author: user._id
-            }, function(err, place) {
-                if (err) {
-                    console.log(err); 
-                    res.render("save", {success: false});
-                    return;
-                }
-                res.render("save", {success: true});
-            });
-        }
-        console.log(req.files);
-        console.log(fs);
         if (req.files.file.size > 0) {
             var ext = req.files.file.name.split(".").pop();
             var path = req.files.file.path;
@@ -92,45 +71,98 @@ app.post("/new-place/save", multipartMiddleware, function(req, res) {
                         console.log(err);
                     }
                 });
-                newPlace(img);
+                createPlace(user, img, req, res);
             });
         } else {
             fs.unlink(req.files.file.path);
-            newPlace(null);
+            createPlace(user, null, req, res);
         }
     });
 });
 
+function createPlace(user, img, req, res) {
+
+    Place.create({
+        name: req.body.name,
+        description: req.body.description,
+        location: {
+            city: req.body.city,
+            zone: req.body.zone,
+            street: req.body.street,
+        },
+        latlng: {
+            latitude: req.body.latitude,
+            longitude: req.body.longitude
+        },
+        images: img ? [img._id]:[],
+        author: user._id //required
+    }, function(err, place) {
+        if (err) {
+            console.log(err); 
+            res.render("save", {success: false});
+            return;
+        }
+        res.render("save", {success: true});
+    });
+}
+
 app.get("/place/:id", function(req, res) {
 
-    console.log(req.params.id);
-    res.render("place");
+    Place.findById(req.params.id, function(err, place) {
+        if (err) {
+            console.log(err);
+            res.render("not-found");
+            return;
+        }
+        User.findById(place.author, function(uerr, author) {
+            if (uerr) console.log(uerr);
+            place.authorName = author.name;
+            if (place.images.length > 0) {
+                Image.findById(place.images[0], function(ferr, image) {
+                    if (ferr) console.log(ferr);
+                    place.image = image._id+"."+image.extension;
+                    console.log(place.image);
+                    res.render("place", {place: place});
+                });
+            } else {
+                place.image = "image.png";
+                console.log(place.image);
+                res.render("place", {place: place});
+            }
+        });
+        
+    });
 });
 
 app.get("/search", function(req, res) {
 
     var regexp = new RegExp(req.query.pattern, "i");
     Place.find({name: regexp}, function(err, data) {
-        console.log(data);
-        function iterate(i, places) {
-            if (i < places.length) {
-                var place = places[i];
-                if (place.images.length > 0) {
-                    Image.findById(place.images[0], function(ferr, image) {
-                        if (ferr) console.log(ferr);
-                        place.image = image._id+"."+image.extension;
-                        iterate(i + 1, places);
-                    });
-                } else{
-                    place.image = "image.png";
-                    iterate(i + 1, places);
-                }
-            } else {
-                res.render("search", {results: data});
-            }
-        }
+        
+        iterateAndShowPlaces(0, data, function() {
+            res.render("search", {results: data});
+        });
     })
 });
+
+function iterateAndShowPlaces(i, places, callback) {
+
+    if (i < places.length) {
+        var place = places[i];
+        if (place.images.length > 0) {
+            Image.findById(place.images[0], function(ferr, image) {
+                if (ferr) console.log(ferr);
+                place.image = image._id+"."+image.extension;
+                iterateAndShowPlaces(i + 1, places, callback);
+            });
+        } else {
+            place.image = "image.png";
+            iterateAndShowPlaces(i + 1, places, callback);
+        }
+    } else {
+        callback();
+    }
+}
 
 app.get("*", function (req, res) {
     res.render("not-found");
